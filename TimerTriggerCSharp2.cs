67cf299969496543a1865617488db119716f8375
LipaOne <lipa@live.com>
2018-05-24 12:32:32 +0200

    dodanie spacji do wiadomości

--- TimerTriggerCSharp2.cs
+++ TimerTriggerCSharp2.cs
@@ -10,7 +10,7 @@ namespace Company.Function
         public static object Run([TimerTrigger("0/10 * * * * *")]TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
-            return new { message = "Message" + myTimer.ScheduleStatus, time = DateTime.Now };
+            return new { message = "Message " + myTimer.ScheduleStatus, time = DateTime.Now };
         }
     }
 }
