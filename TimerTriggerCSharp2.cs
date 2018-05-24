0eab18dab4083f19750eae27fd7b4b4c1570ff70
LipaOne <lipa@live.com>
2018-05-24 12:22:51 +0200

    first azure timer function

---
+++ TimerTriggerCSharp2.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace Company.Function
+{
+    public static class TimerTriggerCSharp2
+    {
+        [FunctionName("TimerTriggerCSharp2")]
+        public static object Run([TimerTrigger("0/10 * * * * *")]TimerInfo myTimer, TraceWriter log)
+        {
+            log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            return new { message = "Message" + myTimer.ScheduleStatus, time = DateTime.Now };
+        }
+    }
+}
